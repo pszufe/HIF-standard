1dca539bff817d74538fa848db35cdb44dbd00c7
Cobord <ammar.s.husain@gmail.com>
2024-08-27 21:16:11 -0400

    use typed dict, make the test cases check why they failed to validate

--- scripts/hif.py
+++ scripts/hif.py
@@ -3,9 +3,11 @@ this script provides a function `validate_hif`
 which returns a dictionary specifying whether every part of the HIF specification is followed.
 """
 
+from __future__ import annotations
 import json
 from collections import defaultdict
-from typing import Dict
+from os import PathLike
+from typing import List, Literal, Optional, TypeAlias,TypedDict, Union
 from warnings import warn
 
 #pylint:disable=missing-function-docstring
@@ -39,61 +41,97 @@ def validate_network_type(data, verbose : bool):
                             "Only maximal faces should be stored for simplicial complexes."
                     )
 
-type SpecificationPart = str
-type StatusCode = int
+SpecificationPart : TypeAlias = str
+StatusCode : TypeAlias = Union[Literal[0],Literal[1]]
 
-#pylint:disable=too-many-branches,too-many-statements
-def validate_hif(path) -> Dict[SpecificationPart,StatusCode]:
+class SpecificationMet(TypedDict):
+    """
+    every part of the HIF specification
+    has a status code
+    """
+    valid_field_names: StatusCode
+    incidences_exist: StatusCode
+    validate_network_type: StatusCode
+    metadata_dict: StatusCode
+    node_record_length: StatusCode
+    node_attr_dict: StatusCode
+    edge_record_length: StatusCode
+    edge_attr_dict: StatusCode
+    incidence_record_length: StatusCode
+    incidence_attr_dict : StatusCode
+
+def all_good() -> SpecificationMet:
+    return SpecificationMet({"valid_field_names":0,
+                        "incidences_exist":0,
+                        "validate_network_type":0,
+                        "metadata_dict":0,
+                        "node_record_length":0,
+                        "node_attr_dict":0,
+                        "edge_record_length":0,
+                        "edge_attr_dict":0,
+                        "incidence_record_length":0,
+                        "incidence_attr_dict":0})
+
+SPECIFICATION_MET_PARTS = len(all_good())
+
+def which_bad(info: SpecificationMet) -> List[str]:
+    return [k for k, v in info.items() if v != 0]
+
+#pylint:disable=too-many-branches,too-many-statements,too-many-locals
+def validate_hif(path : Union[str,PathLike],*,data: Optional[dict] = None) -> SpecificationMet:
     """
     a dictionary specifying whether every part of the HIF specification is followed
     for the file with the given path
+    alternatively, can just provide the loaded data directly and the path will be ignored
     """
+
     #pylint:disable=unspecified-encoding
-    with open(path) as file:
-        # load JSON file
-        data = json.loads(file.read())
+    if data is None:
+        with open(path) as file:
+            # load JSON file
+            data = json.loads(file.read())
 
     # dictionary to store statuses
-    info : Dict[SpecificationPart,StatusCode] = {}
+    info_class = all_good()
 
     # check that keys do not deviate from the standard field names
-    info["valid-field-names"] = 0
+    info_class["valid_field_names"] = 0
     fields = {"network-type", "metadata", "nodes", "edges", "incidences"}
     if not set(data).issubset(fields):
-        fields = ", ".join(fields)
-        data = ", ".join(set(data))
+        fields_warn = ", ".join(fields)
+        data_warn = ", ".join(set(data))
         warn(
-            f"Acceptable field names are: {fields}\nand the field names are {data}"
+            f"Acceptable field names are: {fields_warn}\nand the field names are {data_warn}"
         )
-        info["valid-field-names"] = 1
+        info_class["valid_field_names"] = 1
 
     # incidences are required; check that they exist
-    info["incidences-exist"] = 0
+    info_class["incidences_exist"] = 0
     if "incidences" not in data:
         warn("The file must contain an field for incidences.")
-        info["incidences-exist"] = 1
+        info_class["incidences_exist"] = 1
 
     # check network type
-    info["valid-network-type"] = 0
+    info_class["validate_network_type"] = 0
     network_types = {"asc", "undirected", "directed"}
     if "network-type" in data:
         if data["network-type"] not in network_types:
-            network_types = ", ".join(network_types)
+            network_types_warn = ", ".join(network_types)
             warn(
-                f"Unsupported network type. Valid types are: {network_types}"
+                f"Unsupported network type. Valid types are: {network_types_warn}"
             )
-            info["valid-network-type"] = 1
+            info_class["validate_network_type"] = 1
 
     # check network metadata
-    info["metadata-dict"] = 0
+    info_class["metadata_dict"] = 0
     if "metadata" in data:
         if not isinstance(data["metadata"], dict):
             warn("The metadata must be dict-like.")
-            info["metadata-dict"] = 1
+            info_class["metadata_dict"] = 1
 
     # check node attributes
-    info["node-record-length"] = 0
-    info["node-attr-dict"] = 0
+    info_class["node_record_length"] = 0
+    info_class["node_attr_dict"] = 0
     if "nodes" in data:
         for _i, record in enumerate(data["nodes"]):
             if len(record) != 2:
@@ -101,15 +139,15 @@ def validate_hif(path) -> Dict[SpecificationPart,StatusCode]:
                     " ".join(["Each node record must have two entries:",
                               "an ID and the dictionary of corresponding attributes."])
                 )
-                info["node-record-length"] = 1
+                info_class["node_record_length"] = 1
 
-            if not isinstance(record[1], dict):
+            if len(record)>1 and not isinstance(record[1], dict):
                 warn("The node attributes must be dict-like.")
-                info["node-attr-dict"] = 1
+                info_class["node_attr_dict"] = 1
 
     # check edge attributes
-    info["edge-record-length"] = 0
-    info["edge-attr-dict"] = 0
+    info_class["edge_record_length"] = 0
+    info_class["edge_attr_dict"] = 0
     if "edges" in data:
         for _i, record in enumerate(data["edges"]):
             if len(record) != 2:
@@ -117,15 +155,15 @@ def validate_hif(path) -> Dict[SpecificationPart,StatusCode]:
                     " ".join(["Each edge record must have two entries:",
                               "an ID and the dictionary of corresponding attributes."])
                 )
-                info["edge-record-length"] = 1
+                info_class["edge_record_length"] = 1
 
-            if not isinstance(record[1], dict):
+            if len(record) > 1 and not isinstance(record[1], dict):
                 warn("The edge attributes must be dict-like.")
-                info["edge-attr-dict"] = 1
+                info_class["edge_attr_dict"] = 1
 
     if "incidences" in data:
-        info["incidence-record-length"] = 0
-        info["incidence-attr-dict"] = 0
+        info_class["incidence_record_length"] = 0
+        info_class["incidence_attr_dict"] = 0
 
         for _i, record in enumerate(data["incidences"]):
             if len(record) != 3:
@@ -134,18 +172,18 @@ def validate_hif(path) -> Dict[SpecificationPart,StatusCode]:
                               "an edge ID, a node ID,",
                               "and the dictionary of corresponding attributes."])
                 )
-                info["incidence-record-length"] = 1
+                info_class["incidence_record_length"] = 1
 
-            if not isinstance(record[2], dict):
+            if len(record)>2 and not isinstance(record[2], dict):
                 warn("The incidence attributes must be dict-like.")
-                info["incidence-attr-dict"] = 1
+                info_class["incidence_attr_dict"] = 1
 
     # in the case of directed hypergraphs, each incidence must
     # have the "direction" attribute
     if "network-type" in data and data["network-type"] == "directed":
         data["direction-exists-for-directed"] = 0
         for _i, record in enumerate(data["edges"]):
-            if "direction" not in record[2]:
+            if len(record)<2 or "direction" not in record[2]:
                 warn(
                     " ".join(["Each incidence record must have have",
                              "the 'direction' attribute for directed hypergraphs."])
@@ -168,4 +206,4 @@ def validate_hif(path) -> Dict[SpecificationPart,StatusCode]:
                     )
                     data["maximal-edges-for-asc"] = 1
 
-    return info
+    return info_class
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -4,46 +4,87 @@ all of which are defined in ``conftest.py``
 """
 
 #pylint:disable = missing-function-docstring
+import warnings
 from fastjsonschema import JsonSchemaValueException
 import pytest
 
+from scripts.hif import SPECIFICATION_MET_PARTS, validate_hif, validate_network_type, which_bad
 
 def test_empty(validator, empty):
     with pytest.raises(JsonSchemaValueException):
         validator(empty)
-
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        full_info = validate_hif("",data=empty)
+    assert len(full_info) == SPECIFICATION_MET_PARTS
+    assert which_bad(full_info) == ["incidences_exist"]
 
 def test_bad_top_level_field(validator, bad_top_level_field):
     with pytest.raises(JsonSchemaValueException):
         validator(bad_top_level_field)
-
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        full_info = validate_hif("",data=bad_top_level_field)
+    assert len(full_info) == SPECIFICATION_MET_PARTS
+    assert which_bad(full_info) == ["valid_field_names"]
 
 def test_bad_network_type(validator, bad_network_type):
     with pytest.raises(JsonSchemaValueException):
         validator(bad_network_type)
-
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        full_info = validate_hif("",data=bad_network_type)
+    assert len(full_info) == SPECIFICATION_MET_PARTS
+    assert which_bad(full_info) == ["validate_network_type"]
 
 def test_bad_node_without_id(validator, bad_node_without_id):
     with pytest.raises(JsonSchemaValueException):
         validator(bad_node_without_id)
-
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        full_info = validate_hif("",data=bad_node_without_id)
+    assert len(full_info) == SPECIFICATION_MET_PARTS
+    assert which_bad(full_info) == ["node_record_length"]
 
 def test_single_node(validator, single_node):
     validator(single_node)
-
+    validate_network_type(single_node,False)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        full_info = validate_hif("",data=single_node)
+    assert len(full_info) == SPECIFICATION_MET_PARTS
+    assert which_bad(full_info) == ["node_record_length"]
 
 def test_single_edge(validator, single_edge):
     validator(single_edge)
-
+    validate_network_type(single_edge,False)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        full_info = validate_hif("",data=single_edge)
+    assert len(full_info) == SPECIFICATION_MET_PARTS
+    assert which_bad(full_info) == ["edge_record_length"]
 
 def test_single_incidence(validator, single_incidence):
     validator(single_incidence)
-
+    validate_network_type(single_incidence,False)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        full_info = validate_hif("",data=single_incidence)
+    assert len(full_info) == SPECIFICATION_MET_PARTS
+    assert which_bad(full_info) == ["incidence_record_length"]
 
 def test_metadata_as_list(validator, metadata_as_list):
     with pytest.raises(JsonSchemaValueException):
         validator(metadata_as_list)
-
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        full_info = validate_hif("",data=metadata_as_list)
+    assert len(full_info) == SPECIFICATION_MET_PARTS
+    assert which_bad(full_info) == ["metadata_dict"]
 
 def test_empty_hypergraph(validator, empty_hypergraph):
     validator(empty_hypergraph)
+    validate_network_type(empty_hypergraph,False)
+    full_info = validate_hif("",data=empty_hypergraph)
+    assert len(full_info) == SPECIFICATION_MET_PARTS
+    assert len(which_bad(full_info)) == 0
