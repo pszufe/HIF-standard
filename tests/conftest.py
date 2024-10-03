5c4e47f25bf2042c6f35eb48dd42f83d96c217c6
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-10-03 18:14:16 -0400

    Reorganize unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,31 +7,22 @@ schema = "schemas/hif_schema_v0.1.0.json"
 json_dir = "tests/test_files"
 
 
+# validator
 @pytest.fixture
 def validator():
     return fastjsonschema.compile(json.load(open(schema)))
 
 
+### Examples which should break ###
+
+
+# test empty fields
 @pytest.fixture
 def empty():
     return json.load(open(f"{json_dir}/empty.json", "r"))
 
 
-@pytest.fixture
-def single_node():
-    return json.load(open(f"{json_dir}/single_node.json", "r"))
-
-
-@pytest.fixture
-def single_edge():
-    return json.load(open(f"{json_dir}/single_edge.json", "r"))
-
-
-@pytest.fixture
-def single_incidence():
-    return json.load(open(f"{json_dir}/single_incidence.json", "r"))
-
-
+# test top-level
 @pytest.fixture
 def bad_top_level_field():
     return json.load(open(f"{json_dir}/bad_top_level_field.json", "r"))
@@ -42,16 +33,73 @@ def bad_network_type():
     return json.load(open(f"{json_dir}/bad_network_type.json", "r"))
 
 
-@pytest.fixture
-def bad_node_without_id():
-    return json.load(open(f"{json_dir}/bad_node_without_id.json", "r"))
-
-
+# test metadata
 @pytest.fixture
 def metadata_as_list():
     return json.load(open(f"{json_dir}/metadata_as_list.json", "r"))
 
 
+# test nodes
+@pytest.fixture
+def bad_node_without_id():
+    return json.load(open(f"{json_dir}/bad_node_without_id.json", "r"))
+
+
+# test incidences
+@pytest.fixture
+def bad_incidence_field():
+    return json.load(open(f"{json_dir}/bad_incidence_field.json", "r"))
+
+
+@pytest.fixture
+def single_incidence_with_weight_as_string():
+    return json.load(
+        open(f"{json_dir}/single_incidence_with_weight_as_string.json", "r")
+    )
+
+
+### Examples which should work
+
+
+# empty
 @pytest.fixture
 def empty_hypergraph():
     return json.load(open(f"{json_dir}/empty_hypergraph.json", "r"))
+
+
+# test nodes
+@pytest.fixture
+def single_node():
+    return json.load(open(f"{json_dir}/single_node.json", "r"))
+
+
+@pytest.fixture
+def single_node_with_attrs():
+    return json.load(open(f"{json_dir}/single_node_with_attrs.json", "r"))
+
+
+# test edges
+@pytest.fixture
+def single_edge():
+    return json.load(open(f"{json_dir}/single_edge.json", "r"))
+
+
+@pytest.fixture
+def single_edge_with_attrs():
+    return json.load(open(f"{json_dir}/single_edge_with_attrs.json", "r"))
+
+
+# test incidences
+@pytest.fixture
+def single_incidence():
+    return json.load(open(f"{json_dir}/single_incidence.json", "r"))
+
+
+@pytest.fixture
+def single_incidence_with_weights():
+    return json.load(open(f"{json_dir}/single_incidence_with_weights.json", "r"))
+
+
+@pytest.fixture
+def single_incidence_with_attrs():
+    return json.load(open(f"{json_dir}/single_incidence_with_attrs.json", "r"))
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,11 +1,15 @@
 import pytest
 
+### Tests which check bad formats ###
 
+
+# test empty
 def test_empty(validator, empty):
     with pytest.raises(ValueError):
         validator(empty)
 
 
+# test top-level fields
 def test_bad_top_level_field(validator, bad_top_level_field):
     with pytest.raises(ValueError):
         validator(bad_top_level_field)
@@ -16,27 +20,68 @@ def test_bad_network_type(validator, bad_network_type):
         validator(bad_network_type)
 
 
-def test_bad_node_without_id(validator, bad_node_without_id):
-    with pytest.raises(ValueError):
-        validator(bad_node_without_id)
-
-
-def test_single_node(validator, single_node):
-    validator(single_node)
-
-
-def test_single_edge(validator, single_edge):
-    validator(single_edge)
-
-
-def test_single_incidence(validator, single_incidence):
-    validator(single_incidence)
-
-
+# test metadata
 def test_metadata_as_list(validator, metadata_as_list):
     with pytest.raises(ValueError):
         validator(metadata_as_list)
 
 
+# test nodes
+def test_bad_node_without_id(validator, bad_node_without_id):
+    with pytest.raises(ValueError):
+        validator(bad_node_without_id)
+
+
+# test incidences
+def test_bad_incidence_field(validator, bad_incidence_field):
+    with pytest.raises(ValueError):
+        validator(bad_incidence_field)
+
+
+def test_single_incidence_with_weight_as_string(
+    validator, single_incidence_with_weight_as_string
+):
+    with pytest.raises(ValueError):
+        validator(single_incidence_with_weight_as_string)
+
+
+### Tests which check valid formats
+
+
+# test empty
 def test_empty_hypergraph(validator, empty_hypergraph):
     validator(empty_hypergraph)
+
+
+# test metadata
+
+
+# test nodes
+def test_single_node(validator, single_node):
+    validator(single_node)
+
+
+def test_single_node_with_attrs(validator, single_node_with_attrs):
+    validator(single_node_with_attrs)
+
+
+# test edges
+def test_single_edge(validator, single_edge):
+    validator(single_edge)
+
+
+def test_single_edge_with_attrs(validator, single_edge_with_attrs):
+    validator(single_edge_with_attrs)
+
+
+# test incidences
+def test_single_incidence(validator, single_incidence):
+    validator(single_incidence)
+
+
+def test_single_incidence_with_weights(validator, single_incidence_with_weights):
+    validator(single_incidence_with_weights)
+
+
+def test_single_incidence_with_attrs(validator, single_incidence_with_attrs):
+    validator(single_incidence_with_attrs)
